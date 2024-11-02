95340ba9c0eec00379335259cd90f047f9605691
Tom2021-vice <1340974656@qq.com>
2024-11-02 14:38:22 +0800

    add new font

--- js/table.js
+++ js/table.js
@@ -4,7 +4,31 @@ function addRow() {
     // console.log(table);
     var length = table.rows.length;
     // console.log(length);
+    //获取插入位置
     var newRow = table.insertRow(length);
-    console.log(newRow);
-    //
+    //插入节点对象
+    var dataCol = newRow.insertCell(0);
+    var eventCol = newRow.insertCell(1);
+    var actionCol = newRow.insertCell(2);
+    //修改节点文本内容
+    dataCol.innerHTML = '1';
+    eventCol.innerHTML = '1';
+    actionCol.innerHTML = '<button onclick="editRow(this)">编辑</button><button onclick="deleteRow(this)">删除</button>';
+
+}
+//删除数据函数
+function deleteRow(button){
+    var row = button.parentNode.parentNode;
+    console.log(row);
+    row.parentNode.removeChild(row);
+}
+//编辑数据函数
+function editRow(button){
+    var row = button.parentNode.parentNode;
+    var data = row.cells[0];
+    var event = row.cells[1];
+    var inputdata = prompt("请输入名字：");
+    var inputevent = prompt("请输入事件：");
+    data.innerHTML = inputdata;
+    event.innerHTML = inputevent;
 }
